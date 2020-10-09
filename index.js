3a1c6c34992fb6702357d24686e89ae8ef72e372
besshy8 <toshiki-yasuda0113@keio.jp>
2020-10-10 00:21:44 +0900

    10/10 midnight

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    console.log(message.content);
+    if (message.author.bot) return;
+    // console.log(message.content);
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
     if (message.content === `Afterglow${prefix}`) {
         message.channel.send('えいえいお〜〜');
     } else if (message.content.startsWith('afterglow')) {
@@ -16,6 +19,13 @@ client.on('message', message => {
         message.channel.send(`The server name is : ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     } else if (message.content === 'user_info') {
         message.channel.send(`Your username is : ${message.author.username}\nYour ID is :${message.author.id}`);
+    } else if (command === 'afterglow') {
+        if (!args.length) {
+            message.channel.send('No arumentation');
+        } else if (args[0] === 'Hey!') {
+            message.channel.send('YOLO!!!!');
+        }
+        message.channel.send(`Command Name : ${command}\nArguments : ${args}`);
     }
 });
 

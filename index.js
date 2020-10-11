31bab5a90e1db2cf254b1297a73dabc2c49127e8
besshy8 <toshiki-yasuda0113@keio.jp>
2020-10-11 11:08:08 +0900

    10/11 morning

--- index.js
+++ index.js
@@ -36,6 +36,18 @@ client.on('message', message => {
         const name = mentioned.username;
         const user_id = mentioned.id;
         message.channel.send(`Username : ${name}\nid: ${user_id}`);
+    } else if (command === 'avatar') {
+        if (!message.mentions.users.size) {
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL()}>`);
+        }
+        // const mentioned = message.mentions.users.first();
+        // message.channel.send(`${mentioned.username}'s avatar URL is ${mentioned.displayAvatarURL({ format: 'png', dynamic: false })}`);
+        // console.log(mentioned);
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>\nUsers discriminator is ${user.discriminator}`;
+        });
+        message.channel.send(avatarList);
+
     }
 });
 

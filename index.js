fbe91f3f5e60f57e4edb4e57ac740392a2838276
besshy8 <toshiki-yasuda0113@keio.jp>
2020-10-22 10:19:41 +0900

    10/22 morning

--- index.js
+++ index.js
@@ -48,6 +48,19 @@ client.on('message', message => {
         });
         message.channel.send(avatarList);
 
+    } else if (command === 'prune') {
+        const amount = parseInt(args[0]);
+        // BOTのパーミッション周りでエラーがはかれたので調査
+        if (isNaN(amount)) {
+            return message.reply('that doesn\'t seem to be a valid number.');
+        } else if (amount < 2 || amount > 100) {
+            return message.reply('Invalid Number (Needed 2 to 100)');
+        }
+        // TO DO not console log -> message.channel.send
+        message.channel.bulkDelete(amount, true).catch(err => {
+            console.error(err);
+            message.channel.send('There was an error trying to prune message');
+        });
     }
 });
 

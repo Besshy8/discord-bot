3dd525e48be4681c7ef971bcb05cadf38efcdf28
besshy8 <toshiki-yasuda0113@keio.jp>
2020-10-24 17:10:19 +0900

    modify all command and if/else chain

---
+++ commands/prune.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'prune',
+    description: 'Prune',
+    execute(message, args) {
+        const amount = parseInt(args[0]);
+        // BOTのパーミッション周りでエラーがはかれたので調査
+        if (isNaN(amount)) {
+            return message.reply('that doesn\'t seem to be a valid number.');
+        } else if (amount < 2 || amount > 100) {
+            return message.reply('Invalid Number (Needed 2 to 100)');
+        }
+        // TO DO not console log -> message.channel.send
+        message.channel.bulkDelete(amount, true).catch(err => {
+            console.error(err);
+            message.channel.send('There was an error trying to prune message');
+        });
+    },
+};
--- index.js
+++ index.js
@@ -29,40 +29,14 @@ client.on('message', message => {
         message.channel.send(`The server name is : ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
     } else if (message.content === 'user_info') {
         message.channel.send(`Your username is : ${message.author.username}\nYour ID is :${message.author.id}`);
-    } else if (command === 'afterglow') {
-        if (!args.length) {
-            message.channel.send('No arumentation');
-        } else if (args[0] === 'Hey!') {
-            message.channel.send('YOLO!!!!');
-        }
-        message.channel.send(`Command Name : ${command}\nArguments : ${args}`);
-    } else if (command == 'mention') {
-        client.commands.get('mention').execute(message, args);
-    } else if (command === 'avatar') {
-        if (!message.mentions.users.size) {
-            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL()}>`);
-        }
-        // const mentioned = message.mentions.users.first();
-        // message.channel.send(`${mentioned.username}'s avatar URL is ${mentioned.displayAvatarURL({ format: 'png', dynamic: false })}`);
-        // console.log(mentioned);
-        const avatarList = message.mentions.users.map(user => {
-            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>\nUsers discriminator is ${user.discriminator}`;
-        });
-        message.channel.send(avatarList);
+    }
+    if (!client.commands.has(command)) return;
 
-    } else if (command === 'prune') {
-        const amount = parseInt(args[0]);
-        // BOTのパーミッション周りでエラーがはかれたので調査
-        if (isNaN(amount)) {
-            return message.reply('that doesn\'t seem to be a valid number.');
-        } else if (amount < 2 || amount > 100) {
-            return message.reply('Invalid Number (Needed 2 to 100)');
-        }
-        // TO DO not console log -> message.channel.send
-        message.channel.bulkDelete(amount, true).catch(err => {
-            console.error(err);
-            message.channel.send('There was an error trying to prune message');
-        });
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command');
     }
 });
 
